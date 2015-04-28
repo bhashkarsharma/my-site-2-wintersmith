3ff2ab01c9ded2a40fc89117799179e107b03d48
Bhashkar Sharma <bhashkarsharma@gmail.com>
2015-04-28 13:41:17 +0530

    Adding Connect

---
+++ contents/lab/connect/index.md
@@ -0,0 +1,37 @@
+---
+title: Connect
+template: page.hbs
+description: In-browser chat application
+style: true
+script: true
+---
+A simple peer-to-peer chat application that allows people to watch YouTube videos in sync.
+
+<link rel="stylesheet" href="/css/mediaelementplayer.min.css"/>
+<script src="http://cdn.peerjs.com/0.3/peer.min.js"></script>
+<script src="/js/mediaelement-and-player.min.js"></script>
+<div class="login 6u">
+    <form>
+        <label>Your id is: <input id="username" value="being fetched"></label>
+    </form>
+</div>
+
+<div id="video">
+    <video width="640" height="360" id="player" preload="none">
+        <source type="video/youtube" src="http://www.youtube.com/watch?v=Wch3gJG2GJ4" />
+    </video>
+</div>
+
+<div id="chats"></div>
+
+<div class="row">
+    <div class="3u">
+        <input type="text" name="peername" id="peername" placeholder="Peer name">
+    </div>
+    <div class="8u">
+        <input type="text" name="msg" id="msg" placeholder="Message">
+    </div>
+    <div class="1u">
+        <input type="submit" id="send" value="&gt;">
+    </div>
+</div>
---
+++ contents/lab/connect/script.js
@@ -0,0 +1,212 @@
+var me, conn, player, peername, lasthb, hbwait = 30000, hbstr = "hbt:live:", playstr = "plc:", interval;
+
+$(document).ready(function() {
+    
+    me = new Peer({ key : "sild5qn11u8wu3di" });
+    me.on('open', function(id) {
+        $('#username').val(id);
+    });
+    me.on('connection', function(conn) {
+        conn.on('data', function(data) {
+            chat.processMessage(data, "receive");
+        });
+    });
+    
+    $('#username').on('click', function() {
+        $(this).select();
+    });
+    
+    $('#peername').on('input propertychange paste', function() {
+        peername = $(this).val();
+    });
+    
+    $('form').submit(function() { return false; });
+    
+    $('#send').on('click', function() {
+        var val = chat.removeTags($('#msg').val());
+        if (val.length) {
+            conn = me.connect(peername);
+            conn.on('open', function() {
+                conn.send(val);
+                chat.processMessage(val, "send");
+                $('#msg').val('');
+            });
+        }
+    });
+
+});
+
+// video player
+var video = (function() {
+    var setUp = function() {
+        $("#player").mediaelementplayer({
+//            enablePluginDebug : true,
+            success: function(media, node, playerElem) {
+                player = playerElem.media;
+
+                player.addEventListener("playing", function(e) {
+                    chat.send(playstr + "playing:" + player.currentTime);
+                });
+            },
+            error: function(e) {
+                console.log("err ", e);
+            }
+        });
+    };
+    
+    return { "setUp" : setUp }
+    
+})();
+
+// chat utils
+var chat = (function() {
+
+    var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+    var tagOrComment = new RegExp(
+        '<(?:'
+        // Comment body.
+        + '!--(?:(?:-*[^->])*--+|-?)'
+        // Special "raw text" elements whose content should be elided.
+        + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+        + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+        // Regular name
+        + '|/?[a-z]'
+        + tagBody
+        + ')>',
+        'gi');
+
+    var removeTags = function(html) {
+        var oldHtml;
+        do {
+            oldHtml = html;
+            html = html.replace(tagOrComment, '');
+        } while (html !== oldHtml);
+        return html.replace(/</g, '&lt;');
+    };
+
+    var add = function(msg, direction) {
+        var div = "<div class=\"bubble " + direction + "\">" + msg + "</div>";
+        $('#chats').append(div);
+        $('#chats').scrollTop($('#chats')[0].scrollHeight);
+    };
+    
+    var getYTVidId = function(url) {
+        var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+        return (url.match(p)) ? RegExp.$1 : false;
+    };
+    
+    var processMessage = function(msg, direction) {
+        if (heartbeat.isHB(msg) || plc.isPlayerCode(msg)) {
+            return;
+        }
+        var resp = removeTags(msg);
+        var vidId = getYTVidId(msg);
+        if (vidId) {
+            $('#video').find('source').attr('src', "http://www.youtube.com/watch?v=" + vidId);
+            $('#video').show();
+            video.setUp();
+        }
+        add(resp, direction);
+    };
+    
+    var send = function(msg) {
+        if (peername !== undefined && peername.length > 0) {
+            conn = me.connect(peername);
+            conn.on('open', function() {
+                conn.send(msg);
+            });
+        }
+    };
+
+    return {
+        "send" : send,
+        "removeTags" : removeTags,
+        "processMessage" : processMessage
+    }
+})();
+
+// player code handling
+var plc = (function() {
+    var isPlayerCode = function(val) {
+        if (val.indexOf(playstr) === 0) {
+            processCode(val);
+            return true;
+        }
+        return false;
+    };
+    
+    var processCode = function(code) {
+        var val = code.split(":");
+        if (val[0] + ":" == playstr && player !== undefined) {
+            if(val[1] == "playing") {
+                player.play();
+                player.setCurrentTime(Math.round(val[2] * 100) / 100);
+            }
+        }
+    };
+    
+    return {
+        "isPlayerCode" : isPlayerCode,
+        "processCode" : processCode
+    }
+})();
+
+// heartbeat mgmt
+var heartbeat = (function() {
+
+    var sendAndCheck = function() {
+        if (peername) {
+            chat.send(hbstr + Date.now() + ":" + me.id);
+            if (Date.now() - lasthb > hbwait) {
+                $('#peername').addClass('offline');
+            } else {
+                $('#peername').removeClass('offline');
+            }
+        }
+    };
+    
+    var isHB = function(val) {
+        lasthb = Date.now();
+        if (val.indexOf(hbstr) === 0) {
+            var arr = val.split(":");
+            $('#peername').val(arr[3]);
+            start();
+            return true;
+        }
+        return false;
+    };
+    
+    var start = function() {
+        if (peername !== undefined) {
+            if (interval !== undefined) clearInterval(interval);
+            interval = setInterval(sendAndCheck, 10000);
+        }
+    }
+    
+    start();
+    
+    return {
+        "isHB" : isHB
+    }
+    
+})();
+
+var notifyMe = function(title, body) {
+    if (!Notification) {
+        alert('Please us a modern version of Chrome, Firefox, Opera or Firefox.');
+        return;
+    }
+
+    if (Notification.permission !== "granted") Notification.requestPermission();
+
+    var notification = new Notification('Notification title', {
+        icon: '/images/profile_new.jpg',
+        body: "Hey there! You've been notified!"
+    });
+
+
+    notification.onclick = function () {
+        window.open(".");
+    }
+};
